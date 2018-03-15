e912e1f35658faf37d4dfab64f09179d46b3a8aa
Nikoloz Pirtskhalava <npircxala@gmail.com>
2018-03-15 19:54:11 +0400

    Created Operations Cell Xib With According Class

---
+++ Bog Assignment/Views/Cells/OperationCell.swift
@@ -0,0 +1,24 @@
+//
+//  TransactionCell.swift
+//  Bog Assignment
+//
+//  Created by nikoloz on 15.03.18.
+//  Copyright © 2018 Mobility LLC. All rights reserved.
+//
+
+import UIKit
+
+class OperationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
