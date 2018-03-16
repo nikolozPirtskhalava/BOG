b40c08a40caffba8a546d2e8e87a2647c9e1aae8
Nikoloz Pirtskhalava <npircxala@gmail.com>
2018-03-16 17:40:34 +0400

    Configured EntityGateways and Use Cases For Them.

---
+++ Bog Assignment/Core/Gateways/CollectionGateWay.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionGateWay.swift
+//  Bog Assignment
+//
+//  Created by nikoloz on 16.03.18.
+//  Copyright © 2018 Mobility LLC. All rights reserved.
+//
+
+import Foundation
+
+typealias FetchCollectionDataGatewayCompletionHandler = (_ items: Result<[CollectionModelItem]>) -> Void
+
+protocol CollectionGateWay {
+    func fetchCollectionData(completionHandler: @escaping FetchCollectionDataGatewayCompletionHandler)
+}
---
+++ Bog Assignment/Core/UseCases/DisplayCollectionItemsUseCase.swift
@@ -0,0 +1,9 @@
+//
+//  DisplayCollectionItemsUseCase.swift
+//  Bog Assignment
+//
+//  Created by nikoloz on 16.03.18.
+//  Copyright © 2018 Mobility LLC. All rights reserved.
+//
+
+import Foundation
