05b565ebf3b76bbff8fc26ad5fbdb6d49a0ba1b9
Nikoloz Pirtskhalava <npircxala@gmail.com>
2018-03-17 16:11:01 +0400

    Refactored Logics For Dequeueing Table Cells

--- Bog Assignment/Scenes/Form+List.swift
+++ Bog Assignment/Scenes/Form+List.swift
@@ -11,27 +11,26 @@ import UIKit
 extension FormViewController: UITableViewDelegate, UITableViewDataSource {
     
     func configureTableOnLoad() {
-        self.tableView.register(UINib.init(nibName: Helpers.typeName(for: CollectionViewCell.self), bundle: nil), forCellReuseIdentifier: Helpers.typeName(for: CollectionViewCell.self))
+        self.tableView.register(UINib.init(nibName: Helpers.typeName(for: Helpers.typeName(for: SectionCollectionTableCell.self)), bundle: nil), forCellReuseIdentifier: Helpers.typeName(for: SectionCollectionTableCell.self))
         self.tableView.register(UINib.init(nibName: Helpers.typeName(for: OperationCell.self), bundle: nil), forCellReuseIdentifier: Helpers.typeName(for: OperationCell.self))
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let collectionViewcell = tableView.dequeueReusableCell(withIdentifier: Helpers.typeName(for: CollectionViewCell.self)) as! CollectionViewCell
-        let operationCell = tableView.dequeueReusableCell(withIdentifier: Helpers.typeName(for: OperationCell.self)) as! OperationCell
-        
-        switch self.presenter?.typeFor(section: indexPath.section) {
-        case .CollectionVIew?:
-            self.presenter?.configure(cell: collectionViewcell, forRow: indexPath.row)
+        switch self.presenter!.typeFor(section: indexPath.section) {
+        case .CollectionVIew:
+            let collectionViewcell = tableView.dequeueReusableCell(withIdentifier: Helpers.typeName(for: SectionCollectionTableCell.self)) as! SectionCollectionTableCell
             collectionViewcell.setCollectionViewDataSourceDelegate(self, forRow: indexPath.row)
+            self.presenter?.configure(cell: collectionViewcell, for: indexPath)
             return collectionViewcell
         default:
+            let operationCell = tableView.dequeueReusableCell(withIdentifier: Helpers.typeName(for: OperationCell.self)) as! OperationCell
             self.presenter?.configure(cell: operationCell, for: indexPath)
             return operationCell
         }
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return (self.presenter?.numberOfSections)!
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
