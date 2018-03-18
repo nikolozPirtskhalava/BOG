d0f50444d2d1cb0fa607ba90ce339cd45ec49e76
Nikoloz Pirtskhalava <npircxala@gmail.com>
2018-03-18 14:06:38 +0400

    Added Row+Section Extracting Functions From Tag In Helpers.swift

--- Bog Assignment/Other/Helpers/Helpers.swift
+++ Bog Assignment/Other/Helpers/Helpers.swift
@@ -10,7 +10,21 @@ import Foundation
 
 struct Helpers {
     
+    static let multiplier = 1000
+    
     public static func typeName(for object: Any) -> String {
         return String.init(describing: object)
+    }
+    
+    public static func rowFromTag(tag: Int) -> Int {
+        return tag % multiplier
+    }
+    
+    public static func sectionFromTag(tag: Int) -> Int {
+        return tag / multiplier
+    }
+    
+    public static func tagFromIndexPath(indexPath: IndexPath) -> Int {
+        return (indexPath.section * multiplier) + indexPath.row
     }
 }
