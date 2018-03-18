adb8a52abb2eacba6f394473a40949ec72697082
Nikoloz Pirtskhalava <npircxala@gmail.com>
2018-03-18 13:56:41 +0400

    Added Icons To Assets For Categories

---
+++ Bog Assignment/Other/Views/Headers/OperationHeader.swift
@@ -0,0 +1,21 @@
+//
+//  OperationHeader.swift
+//  Bog Assignment
+//
+//  Created by nikoloz on 18.03.18.
+//  Copyright © 2018 Mobility LLC. All rights reserved.
+//
+
+import UIKit
+
+class OperationHeader: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Bog Assignment/Supporting Files/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Bog Assignment
+//
+//  Created by nikoloz on 18.03.18.
+//  Copyright © 2018 Mobility LLC. All rights reserved.
+//
+
+import Foundation
